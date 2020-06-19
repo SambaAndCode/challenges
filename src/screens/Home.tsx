2c2b43f548f87d81a8998bc9044875ae1997e385
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-06-19 02:02:16 -0300

    add initial setup

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+import api from '../services/api';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
